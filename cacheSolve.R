6e3992f83f7359131587c2250ebffd9a182406a6
Gabriel V Olivares Guillén <gvog75@gmail.com>
2021-01-28 16:45:28 -0600

    Update cacheSolve.R

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,4 +1,7 @@
 cacheSolve<- function(x, ...) {
+  # The following function corresponds to the Programming Assignment 2: Lexical Scoping
+  # This function corresponds the cache function that is explained in the "MakeCacheMatrix" which  
+  # stores the inverse of a Matrix.
   m <- x$getsolve()
   if(!is.null(m)) {
     message("getting cached data")
