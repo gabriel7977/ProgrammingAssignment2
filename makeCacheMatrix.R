7eda21bd9afcebee278a50f7d40fb7f5c2c7ea4a
Gabriel V Olivares Guillén <gvog75@gmail.com>
2021-01-28 16:44:26 -0600

    Update makeCacheMatrix.R
    THIS CORRECTS AND INCLUDES THE COMMENTS RECEIVED

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,4 +1,9 @@
 makeCacheMatrix <- function(x = Matrix()) {
+# The following function corresponds to the Programming Assignment 2: Lexical Scoping
+# This function calculates the inverse of a matrix (IM).
+#However it first checks to see if the IM has already been calculated. 
+#If this is the case, it gets the inverse  from the "cache function" (CacheSolve.R) and skips the computation. 
+# Otherwise, it calculates the IM and sets the value of the IM the cache via the setmean function.
   m <- NULL
   set <- function(y) {
     x <<- y
